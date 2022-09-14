2687be934ad239d1db7decf7a013c9bbf88501f1
Devin Daniels <devingdaniels@gmail.com>
2022-09-14 07:51:30 -0700

    Updated UI

--- src/populateTodayWeather.js
+++ src/populateTodayWeather.js
@@ -1,4 +1,4 @@
-// import Fahrenheit from './assets/fahrenheit.svg';
+import Fahrenheit from './assets/fahrenheit.svg';
 // import Celsius from './assets/celsius.svg';
 // import ThunderStorm from './assets/thunderstorm.svg';
 // import Drizzle from './assets/drizzle.svg';
@@ -7,6 +7,11 @@
 // import Sun from './assets/sun.svg';
 import City from './assets/city.svg';
 import Location from './assets/location.svg';
+import Thermostat from './assets/thermostat.svg';
+import Wind from './assets/wind.svg';
+import Speed from './assets/speed.svg';
+import Humidity from './assets/humidity.svg';
+import Percentage from './assets/percentage.svg';
 
 // function todayWeatherIcon(val) {
 //   const value = Number(val);
@@ -33,29 +38,45 @@ import Location from './assets/location.svg';
 //   return Sun;
 // }
 
-// function weatherDataItem(img, heading, data) {
-//   const container = document.createElement('div');
-//   container.classList.add('todayWeatherItemContainer');
-//   const image = new Image();
-//   image.src = img;
-//   image.classList.add('todayWeatherItemImage');
+function createWeatherWidget(img, heading, data, unitImg) {
+  const container = document.createElement('div');
+  container.classList.add('todayWeatherItemContainer');
+  const image = new Image();
+  image.src = img;
+  image.classList.add('todayWeatherItemImage');
 
-//   const infoContainer = document.createElement('div');
-//   infoContainer.classList.add('todayWeatherItemInfo');
-//   if (heading !== '') {
-//     const title = document.createElement('p');
-//     title.innerHTML = heading;
-//     infoContainer.append(title);
-//   }
-//   const dataInfo = document.createElement('h3');
-//   dataInfo.innerHTML = data;
-//   infoContainer.append(dataInfo);
+  const infoContainer = document.createElement('div');
+  infoContainer.classList.add('todayWeatherItemInfo');
+  if (heading !== '') {
+    const title = document.createElement('p');
+    title.classList.add('todayInfoLabel');
+    title.innerHTML = heading;
+    infoContainer.append(title);
+  }
 
-//   container.append(image);
-//   container.append(infoContainer);
+  // Data with unit image
+  const dataUnitContainer = document.createElement('div');
+  dataUnitContainer.classList.add('dataInfoAndUnit');
+  // The text
+  const dataInfo = document.createElement('h3');
+  dataInfo.innerHTML = data;
+  dataUnitContainer.append(dataInfo);
+  // The image
+  if (unitImg !== '') {
+    const unit = document.createElement('div');
+    unit.style.filter = 'invert(1)';
+    const unitImage = new Image();
+    unitImage.src = unitImg;
+    // Appends
+    unit.append(unitImage);
+    dataUnitContainer.append(unit);
+  }
+  infoContainer.append(dataUnitContainer);
+  container.append(image);
+  container.append(infoContainer);
 
-//   return container;
-// }
+  return container;
+}
 function createCityItem(cityName) {
   // Container for city name and city image
   const el = document.createElement('div');
@@ -104,16 +125,20 @@ function populateLeftSection(data) {
   latitudeLongitude.append(createLatLonItem(data.lat, data.lon));
 }
 
-export default function populateTodayWeatherData(data) {
-  populateLeftSection(data);
+function populateRightSection(data) {
+  const todayFeelsLike = document.getElementById('today-feels-like');
+  const todayWindSpeed = document.getElementById('today-wind-speed');
+  const todayHumidity = document.getElementById('today-humidity');
+  todayFeelsLike.append(createWeatherWidget(Thermostat, 'Feels Like', data.feelsLike, Fahrenheit));
+  todayWindSpeed.append(createWeatherWidget(Wind, 'Wind Speed', data.windSpeed, Speed));
+  todayHumidity.append(createWeatherWidget(Humidity, 'Humidity', data.humidity, ''));
 }
 
-// function populateRightSection() {
-//   // Right section
-//   const todayFeelsLike = document.getElementById('today-feels-like');
-//   const todayWindSpeed = document.getElementById('today-wind-speed');
-//   const todayHumidity = document.getElementById('today-humidity');
-// }
+export default function populateTodayWeatherData(data) {
+  populateLeftSection(data);
+  populateRightSection(data);
+}
+
 // function populateMiddleSection(data) {
 // // Middle section
 //   const todayIcon = document.getElementById('today-icon');
